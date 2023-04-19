ea8d55cb677cad146f6716afa53ae4f5fc6ce1cf
Hueylove <40637131+Hueylove@users.noreply.github.com>
2023-04-19 15:27:18 +0800

    Update fun_test.py
    Modified the format of the code

--- fun_test.py
+++ fun_test.py
@@ -1,21 +1,21 @@
 pipeline
 {
-        agent any
-        stages{
-                    stage("first"){
-                                    steps {
-                                             echo 'hello world'
-                                          }
-                                  }
-                           stage("run test"){
-                                    steps {
-                                        echo 'run test' 
-                                        }
-                                  }
-            }
+    agent any
+    stages{
+           stage("first"){
+               steps {
+                          echo 'hello world'
+               }
+           }
+           stage("run test"){
+               steps {
+                           echo 'run test' 
+               }
+           }
+           }
                 post{
-                            always{
-                                          echo 'always say goodbay'
+                           always{
+                                      echo 'always say goodbay'
                                   }
                      }
 }
